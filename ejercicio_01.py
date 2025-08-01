d72a48a83217aa92ef8bc816e86b3c7a2f33e27b
Maykmora <maykmora12@gmail.com>
2025-08-01 09:51:05 -0600

    commit 3

--- ejercicio_01.py
+++ ejercicio_01.py
@@ -1,13 +1,17 @@
 estudiantes={}
-
+notas=[]
 try:
     n=int(input("Ingrese la cantidad de estudiantes que desea registrar: "))
     for i in range(n):
-        nombre=input("Ingrese el nombre del estudiante:")
+        nombre=input(f"Ingrese el nombre del estudiante #{i+1}:")
         cantidad=int(input("Ingrese la cantidad de notas para agregar: "))
         for j in range(cantidad):
-            nota=int(input(f"Ingrese la nota No.{i+1} del estudiante: "))
-    estudiantes[nombre]={"notas":nota}
+            nota=int(input(f"Ingrese la nota No.{j+1} del estudiante: "))
+            notas.append(nota)
+    estudiantes[nombre]={"notas":[nota]}
+
+    print("\n--NOTAS--")
+    for
 
 except Exception as e:
     print("Ocurrio un erro inesperado")
